7636a31601113c7dcdc010b241530e92752eed41
Simon <lester.simon.b@gmail.com>
2016-06-21 17:47:10 +0200

    Add web server Api

---
+++ PPKWebServer/PPKWebServer/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PPKWebServer
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
